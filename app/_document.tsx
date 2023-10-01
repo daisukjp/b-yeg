3bfd5af268f87b1b8e3693a95bc40a1c5cee7a38
Daisuke Sato <daisukjp1@gmail.com>
2023-09-30 23:09:03 -0600

    added

---
+++ app/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google tag (gtag.js) の追加 */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-RN4RPNSGSD"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-RN4RPNSGSD');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
