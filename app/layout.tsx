b60d5e3c19790787a441f1200f19fbeda4213242
Daisuke Sato <daisukjp1@gmail.com>
2023-09-30 22:34:12 -0600

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,10 @@ import Head from "./head";
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "POPYEG",
-//   description: "Blog web app by Daisuke Sato",
-// };
+export const metadata: Metadata = {
+  title: "POPYEG",
+  description: "Blog web app by Daisuke Sato",
+};
 
 export default function RootLayout({
   children,
@@ -18,6 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head />
+      <meta
+        name="google-site-verification"
+        content="bYkwGSaKwfcCY31tXUOH3lvfNwz46pkKBoNYdwZBke0"
+      />
       <body
         className={inter.className}
         style={{ backgroundColor: "hsl(0, 0%, 98%)" }}
