d6e89a8837551ee9bd41a9016b71233c7b2c8c4c
Daisuke Sato <daisukjp1@gmail.com>
2023-09-30 23:13:34 -0600

    added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Header from "./header";
 import getPostMetaData from "@/components/getPostMetaData";
 import PostPreview from "@/components/PostPreview";
+import Script from "next/script";
 
 export default function Home() {
   const postMetaData = getPostMetaData();
@@ -12,6 +13,17 @@ export default function Home() {
   return (
     <main>
       <Header />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-RN4RPNSGSD" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-RN4RPNSGSD');
+        `}
+      </Script>
+
       <div>
         <main className="px-6 md:px-6 pt-16 pb-24 md:pt-20 md:pb-44 max-w-[700px] mx-auto ring-offset-primary ">
           {postPreviews}
