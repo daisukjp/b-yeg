a3c228a9ce46c3854fcd7853de0f01496a3d466f
Daisuke Sato <daisukjp1@gmail.com>
2023-10-02 21:36:28 -0600

    added chnage

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,14 @@ export default function Head() {
     <>
       <title>POPYEG</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Blog web app by Daisuke" />
+      <meta
+        name="description"
+        content="Explore a captivating journey through NAIT, a renowned college in Edmonton, Alberta, with our blog. Dive into exclusive insights, local information, and student life uniquely curated for your reading pleasure!"
+      />
+      <meta
+        name="keywords"
+        content="Delve into NAIT college life through our Edmonton-based blog, offering exclusive insights into student experiences, local Alberta information, and campus happenings, all thoughtfully curated for our readers, Edmonton, Canada, Alberta, NAIT, エドモントン, カレッジ, カナダ, アルバータ,"
+      ></meta>
       <meta property="og:title" content="POPYEG" />
       <meta property="og:description" content="Blog web app by Daisuke" />
       {/* ?google */}
@@ -11,7 +18,7 @@ export default function Head() {
         name="google-site-verification"
         content="bYkwGSaKwfcCY31tXUOH3lvfNwz46pkKBoNYdwZBke0"
       />
-      <link rel="icon" href="E-logo.png" />
+      <link rel="icon" href="favicon.ico" />
     </>
   );
 }
